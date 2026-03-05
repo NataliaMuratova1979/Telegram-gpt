11b146a917c61189e1cf0c0b6a15a7ae6d52c982
NataliaMuratova1979 <mno@mail.ru>
2026-03-05 13:52:34 +0300

    Создан компонент CategoryButtons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Title } from './shared/ui/Title';
 import { Button } from './shared/ui/Button';
-import ColorDivs from './shared/ui/ColorDivs';
-import ColorfulButtons from './shared/ui/ColorfulButtons';
-import ThemesButtons from './shared/ui/ThemesButtons';
 import './app/styles/index.css';
 import { CloseButton } from './shared/ui/CloseButton';
-import ButtonThemes from './api/mockExample';
+import CategoryButtons from './api/mockExample';
 import Modal from './shared/ui/Modal';
 import CheckBox from './shared/ui/CheckBox';
 import RadioBox from './shared/ui/RadioBox';
@@ -88,36 +85,12 @@ export const App: React.FC = () => {
   return (
     <div style={{ padding: 20 }}>
       
-         {/* ThemesButtons */}
-      <div style={{ marginBottom: '20px' }}>
-        <h3>ThemesButtons</h3>
-        <ThemesButtons
-           onThemeSelect={(theme) =>
-    handleThemeSelect(theme, setFormData, setSelectedTheme, setIsModalOpen)()
-  }
-        />
-      </div>
+    
 
       {/* Кнопка открытия модального */}
       <div style={{ marginBottom: '20px' }}>
         <p>Кнопка для открытия модального окна</p>
         <button onClick={() => setIsModalOpen(true)}>Открыть модальное окно</button>
-      </div>
-
-      {/* Радиобокс */}
-      <div style={{ marginBottom: '20px' }}>
-        <h3>Выберите опцию:</h3>
-        <RadioBox
-          options={[
-            { label: 'Опция 1', value: 'option1' },
-            { label: 'Опция 2', value: 'option2' },
-            { label: 'Опция 3', value: 'option3' },
-          ]}
-          selectedValue={radioValue}
-          onChange={handleRadio}
-          name="sampleRadio"
-          disabled={false}
-        />
       </div>
 
       {/* Выбор по длине */}
@@ -157,58 +130,17 @@ export const App: React.FC = () => {
         )}
       </div>
 
-            {/* Модальное окно */}
-      <div style={{ marginBottom: '20px' }}>
-        <h3>Модальное окно</h3>
-        <Modal
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-          title="Тема выбрана"
-          modalType="option"
-        >
-          <p>Вы выбрали тему: {selectedTheme}</p>
-        </Modal>
-      </div>
 
-            {/* ThemesButtons */}
-      <div style={{ marginBottom: '20px' }}>
-        <h3>ThemesButtons</h3>
-        <ThemesButtons
-  onThemeSelect={(theme) => handleThemeSelect(theme, setFormData, setSelectedTheme, setIsModalOpen)}
-/>
-      </div>
       
-      
-      {/* Модальное окно */}
-      <div style={{ marginBottom: '20px' }}>
-        <h3>Модальное окно</h3>
-        <Modal
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-          title="Тема выбрана"
-          modalType="option"
-        >
-          <p>Вы выбрали тему: {selectedTheme}</p>
-        </Modal>
-      </div>
-
-        {/* Заголовки */}
-      <div style={{ marginBottom: '20px' }}>
-        <Title as="h1">Заголовок</Title>
-        <Title as="h2">Чередование цвета для списка кнопок</Title>
-      </div>
-
-      {/* Цветные кнопки */}
-      <div style={{ marginBottom: '20px' }}>
-        <h3>Цветные кнопки</h3>
-        <ColorfulButtons />
-      </div>
-
-
           {/* ButtonThemes */}
       <div style={{ marginBottom: '20px' }}>
-        <h3>ButtonThemes</h3>
-        <ButtonThemes />
+        <h4>CategoryButtons, mockExample</h4>
+        <CategoryButtons
+  onCategorySelect={(data) => {
+    console.log('Выбранная категория:', data.category);
+    console.log('Темы:', data.topics);
+  }}
+/>
       </div>
 
          {/* Close buttons */}
@@ -220,40 +152,6 @@ export const App: React.FC = () => {
         />
       </div>
 
-
-          <div style={{ marginBottom: '20px' }}>
-        <h3>CloseButton (Выход)</h3>
-        <CloseButton
-          actionType="exit"
-          onExit={() => console.log('Выход из приложения')}
-        />
-      </div>
-
-      {/* Генерация 20 кнопок */}
-      <div style={{ marginBottom: '20px' }}>
-        <h3>Генерация 20 кнопок</h3>
-        {buttonsData.map((btn, idx) => (
-        <Button
-    key={idx}
-    variant="colorful"
-    index={idx}
-    style={{ marginRight: 10, marginBottom: 10 }}
-    onClick={() => console.log(`Пользователь выбрал тему "${btn.label}"`)}
-  >
-    {btn.label}
-  </Button>
-        ))}
-      </div>
-
-      {/* Модальное окно */}
-      <Modal isOpen={isModalOpen} modalType="option" onClose={() => setIsModalOpen(false)}>
-        {/* Тут можно разместить содержимое модалки */}
-        <CloseButton onClick={() => setIsModalOpen(false)} />
-        <h2>Модальное окно</h2>
-        {/* Например, можно разместить сюда форму или другую логику */}
-      </Modal>
-      
-    
       
     </div>
   );
